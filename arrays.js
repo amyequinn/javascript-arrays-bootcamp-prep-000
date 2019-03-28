83bea7fd50ba514d05acae2dd6d2b109096a377e
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-28 14:23:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,10 @@ addElementToBeginningOfArray.unshift("foo");
 
 return addElementToBeginningOfArray
 }
+
+function destructivelyAddElementToBeginningOfArray(){
+  var destructivelyAddElementToBeginningOfArray = [1];
+  destructivelyAddElementToBeginningOfArray.unshift("foo");
+  return destructivelyAddElementToBeginningOfArray
+  
+}
