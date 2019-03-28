0311f207fa2f6100b7ab4a1429d2f0644d892818
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-28 14:46:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,11 +2,8 @@
 var chocolateBars=["snickers", "hundred grand", "kitat", "skittles"];
 
 function addElementToBeginningOfArray(){
-
 var addElementToBeginningOfArray = [1];
-
 addElementToBeginningOfArray.unshift("foo");
-
 return addElementToBeginningOfArray
 }
 
