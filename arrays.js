5381488254916c872227ee6fd725ea0f61001797
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-28 14:34:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,14 +6,14 @@ function addElementToBeginningOfArray(){
 var addElementToBeginningOfArray = [1];
 
 addElementToBeginningOfArray.unshift("foo");
-const array = [1]
+const array = [1];
 return addElementToBeginningOfArray
 }
 
 function destructivelyAddElementToBeginningOfArray(){
   var destructivelyAddElementToBeginningOfArray = [1];
   destructivelyAddElementToBeginningOfArray.unshift("foo");
-
+  const array = [1];
   return destructivelyAddElementToBeginningOfArray
 
 }
