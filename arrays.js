075d8a81319a1046eb285b8db2bfe509dc4dd654
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-28 14:20:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,12 @@
-var chocolateBars = ["snickers", "hundred grand", "kitat", "skittles"];
+
+var chocolateBars=["snickers", "hundred grand", "kitat", "skittles"];
+
+function addElementToBeginningOfArray(){
+
+var addElementToBeginningOfArray = [1];
+
+addElementToBeginningOfArray.unshift("foo");
+
+return addElementToBeginningOfArray
+}
+
